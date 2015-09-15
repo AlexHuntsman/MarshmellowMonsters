67c447e3085ac0cd3d16d0828c97f1041e687a97
Alexander Huntsman <Huntsman.alex101@gmail.com>
2015-09-15 09:57:44 -0600

    Added Getters Setters and a Scanner

--- src/marshmellows/controller/MonsterController.java
+++ src/marshmellows/controller/MonsterController.java
@@ -2,11 +2,12 @@ package marshmellows.controller;
 
 import marshmellows.model.MarshmallowMonster;
 import marshmellows.view.MarshmallowOutput;
-
+import java.util.Scanner;
 public class MonsterController
 	{
 		private MarshmallowMonster alexMonster;
 		private MarshmallowOutput myOutput;
+		private Scanner monsterScanner;
 		public MonsterController()
 		{
 			int eyes = 3;
@@ -17,6 +18,7 @@ public class MonsterController
 			String name = "MR Spidder";
 			int Stinger = 1;
 			
+			monsterScanner = new Scanner(System.in);
 			myOutput = new MarshmallowOutput();
 			alexMonster = new MarshmallowMonster(name, eyes, noses,hair,legs,hasBellyButton, Stinger);
 			
@@ -24,5 +26,14 @@ public class MonsterController
 		public void start()
 		{
 			myOutput.displayMonsterInfo(alexMonster.toString());
+			askQuestions();
+			myOutput.displayMonsterInfo("New Monster Info" + alexMonster.toString());
+		}
+		private void askQuestions()
+		{
+			System.out.println("Type in a better name or the monster");
+			String betterMonsterName = monsterScanner.next();
+			alexMonster.setMonsterName(betterMonsterName);
+			
 		}
 	}
--- src/marshmellows/model/MarshmallowMonster.java
+++ src/marshmellows/model/MarshmallowMonster.java
@@ -9,6 +9,7 @@ public class MarshmallowMonster
 		private double monsterLegs;
 		private boolean monsterBellyButton;
 		private int monsterStinger;
+		
 		private MarshmallowMonster()
 		{
 			
@@ -24,6 +25,67 @@ public class MarshmallowMonster
 			this.monsterStinger = monsterStinger;
 			
 		}
+		//Getters & Setters
+		public String getMonsterName()
+		{
+			return monsterName;
+		}
+		
+		public int getMonsterEyes()
+		{
+			return monsterEyes;
+		}
+		public int getMonsterNoses()
+		{
+			return monsterNoses;
+		}
+		public double getMonsterHair()
+		{
+			return monsterHair;
+		}
+		public double getMonsterLegs()
+		{
+			return monsterLegs;
+		}
+		public boolean getMonsterBellyButton()
+		{
+			return monsterBellyButton;
+		}
+		public int getMonsterStinger()
+		{
+			return monsterStinger;
+		}
+		
+		//Setter
+		public void setMonsterName(String monsterName)
+		{
+			this.monsterName = monsterName;
+		}
+		public void setMonsterEyes(int monsterEyes)
+		{
+			this.monsterEyes = monsterEyes;
+		}
+		public void setMonsterNoses(int monsterNoses)
+		{
+			this.monsterNoses = monsterNoses;
+		}
+		public void setMonsterHair(double monsterHair)
+		{
+			this.monsterHair = monsterHair;
+		}
+		public void setMonsterLegs(double monsterLegs)
+		{
+			this.monsterLegs = monsterLegs;
+		}
+		public void setMonsterBellyButton(boolean monsterBellyButton)
+		{
+			this.monsterBellyButton = monsterBellyButton;
+		}
+		public void setMonsterName(int monsterStinger)
+		{
+			this.monsterStinger = monsterStinger;
+		}
+		
 		public String toString()
 		{
 			String monster = "This Monster has " + monsterEyes + " eyes, " + monsterNoses + " nose(s), and " + monsterLegs + " legs, and it is " + monsterBellyButton + " that he has a belly button also he has " + monsterStinger + " stinger(s), and its' name is " + monsterName;
